c20c34541ff0cabff108720fbc06c3c1ea5c4698
jaewoo-so <nchos88@gmail.com>
2022-11-29 11:13:26 +0900

    Update delall.sh

--- delall.sh
+++ delall.sh
@@ -7,12 +7,10 @@ microk8s.kubectl delete deployment.apps/postneralldp
 microk8s.kubectl delete deployment.apps/insertdbdp     
 microk8s.kubectl delete deployment.apps/mecabdp        
 
-
 microk8s.kubectl delete service/getdb-lb         
 microk8s.kubectl delete service/pre1-lb          
 microk8s.kubectl delete service/insertdb-lb      
 microk8s.kubectl delete service/mecab-lb         
 microk8s.kubectl delete service/pn-lb            
 microk8s.kubectl delete service/catlocal-lb      
-microk8s.kubectl delete service/postnerall-lb    
-microk8s.kubectl delete service/cronjobtest-lb   
+microk8s.kubectl delete service/postnerall-lb      
